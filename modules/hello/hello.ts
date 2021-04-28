4b6d4c0d1ce4ecf9395a598434b1245f7488a4af
ryo-i <iidazz0109@gmail.com>
2021-04-29 07:09:12 +0900

    helloモジュールをfetch APIに変更

--- modules/hello/hello.ts
+++ modules/hello/hello.ts
@@ -1,16 +1,27 @@
-import { message } from './message';
+const url = 'data/hello.json';
 
-const text: string = message.text;
-const selector: string = message.selector;
+async function hello () {
+    try {
+        const res = await fetch(url);
+        const hello = await res.json();
+        const text: string = hello.message.text;
+        const selector: string = hello.message.selector;
+        const dom: HTMLButtonElement = document.querySelector(selector) as HTMLButtonElement;
 
+        if (!text) {
+            throw new Error('テキストが見つからないYo！');
+        } else if (!selector) {
+            throw new Error('セレクタ名が見つからないYo！');
+        } else if (!dom) {
+            throw new Error('DOMが見つからないYo！');
+        }
 
-const hello = (): void => {
-    const dom: HTMLButtonElement = document.querySelector(selector) as HTMLButtonElement;
-    console.log(dom);
-    if (dom) {
         dom.innerHTML = text;
         console.log('text-> ' + text);
+      } catch(err) {
+        console.log('err!');
+        console.log(err);
     }
-}
+};
 
 export { hello };
